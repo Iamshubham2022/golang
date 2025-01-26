d5ce8dd6564eff1de4a5fedaa58af2070caaee92
Iamshubham2022 <shubhamjaiswal14565220@gmail.com>
2025-01-26 15:52:00 +0530

    d

--- json/main.go
+++ json/main.go
@@ -1,5 +1,20 @@
 package main
 
+import "fmt"
+
+var person struct{
+	Name string `json:"name"`
+	Age int `json:"age"`
+	Is_Adult int `json:"is_adult"`
+}
+
+var student struct{
+	Name string `json:"name"`
+	Age int `json:"age"`
+	RollNo int `json:"rollno"`
+}
+
 func main(){
-	
+	fmt.Println("we are learing golang")
+
 }
