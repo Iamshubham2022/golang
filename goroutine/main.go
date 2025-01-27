926f540e4c909ed56e3025c0b14e354c0a9dea55
Iamshubham2022 <shubhamjaiswal14565220@gmail.com>
2025-01-27 22:50:27 +0530

    check

--- goroutine/main.go
+++ goroutine/main.go
@@ -5,24 +5,37 @@ import (
 	"time"
 )
 
-func Addation(a, b int){
+func Addation(a, b int) {
 	fmt.Println(a + b)
-	time.Sleep(1000*time.Millisecond)
+	time.Sleep(1000 * time.Millisecond)
 	fmt.Println("hii this is from the additions")
 }
 
-func Subtraction(a, b int){
+func Subtraction(a, b int) {
 	fmt.Println(a - b)
 	time.Sleep(2000 * time.Millisecond)
 	fmt.Println("hii this is from the subtractions")
-	
+
+}
+
+func printsomthing(message string) {
+	for i := 1; i <= 20; i++ {
+		fmt.Println(message, i)
+		time.Sleep(1000 * time.Millisecond)
+		if i == 6 {
+			fmt.Println("hii this is from the printsomthing function")
+		}
+	}
 }
 
 func main() {
-	fmt.Println("learing goroutine and checking how concurrency works and parallelism,so lets start")
-	go Addation(5, 6)
-	go Subtraction(7, 3)
+	// fmt.Println("learing goroutine and checking how concurrency works and parallelism,so lets start")
+	// go Addation(5, 6)
+	// go Subtraction(7, 3)
+	// time.Sleep(2000 * time.Millisecond)
 
-	time.Sleep(2000 * time.Millisecond)
+	go printsomthing("hello world")
+
+	printsomthing("main function")
 
 }
